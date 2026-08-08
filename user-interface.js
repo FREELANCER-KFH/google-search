2f3139afc240a3ce3fcb7bfdf4d8b22597da3c50
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-08-08 14:53:16 +0000

    buuuu

--- user-interface.js
+++ user-interface.js
@@ -2,4 +2,4 @@ import {btnAdjuntarArchivos} from './elements/buttons/btn-adjuntar-archivos.js'
 
 const root = document.getElementById('root')
 
-root.appendChild(btnAdjuntarArchivos)
+root.appendChild(btnAdjuntarArchivos())
