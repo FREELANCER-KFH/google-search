94a1595a22f9b76e39a69673de2470b16122f65c
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2026-08-08 14:48:17 +0000

    klk

---
+++ elements/buttons/btn-adjuntar-archivos.js
@@ -0,0 +1,14 @@
+export const btnAdjuntarArchivos = () => {
+    const element = document.createElement('button')
+    element.classList.add('.boton')
+    element.id = 'btn-adjuntar-archivo'
+    element.textContent = '+'
+    element.addEventListener('click', adjuntar_archivos)
+
+    return element
+}
+
+const adjuntar_archivos = () => {
+    alert('este es un mensaje de prueba')
+    console.log('este es un mensaje de prueba')
+}
---
+++ user-interface.js
@@ -0,0 +1,6 @@
+import {btnAdjuntarArchivo} from './elements/buttons/btn-adjuntar-archivos.js'
+
+const root = document.getElementById('root')
+
+
+root.appendChild(btnAdjuntarArchivo)
